b42d64a34e0c258d7bfc2d2d0a39e20e09091be8
Code-keys <51513719+Code-keys@users.noreply.github.com>
2019-09-12 16:34:44 +0800

    Add files via upload

--- read.py
+++ read.py
@@ -1,46 +1,72 @@
 import numpy as np
-def imshow(data,mun):
+
+def imshow(img,lable,mun):
+    """
     # 显示img数字
+    :param data: image
+    :param mun: show num
+    :return: 0
+    """
     import matplotlib.pyplot  as plt
     for i in range(mun):
-        img = np.resize(data[i], (28, 28))
-        plt.imshow(img)
+        img1 = np.resize(img[i], (28, 28))
+        plt.imshow(img1)
+        print(lable[i])
         plt.show()
     return
 
-def onehot(loaded_y):  # \编码
+def onehot(loaded_y):  # \编码  class
+    """
+    :param loaded_y:
+    :return: coded
+    """
     D = {"0": [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], "1": [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], "2": [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
          "3": [0, 0, 0, 1, 0, 0, 0, 0, 0, 0], "4": [0, 0, 0, 0, 1, 0, 0, 0, 0, 0], "5": [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
          "6": [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], "7": [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], "8": [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
          "9": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]}
 
-    loady = []
+    loady = list()
     for i in range(len(loaded_y)):
         change = str(loaded_y[i])
         loady.append(D[(change)])
     return loady
 
 def readdata():
-    '''
-    读取mnist图像数据 类似bmp文件
+    """
+      读取mnist图像数据 类似bmp文件
     :return: 数据/流
-    '''
+    """
     # 加载二进制数据
-    with open('./data/train-images-idx3-ubyte','rb') as x :
-        with open('./data/train-labels-idx1-ubyte', 'rb') as  y :
-            # 解析
-            x.read(16)#所在的位置开始读16个字节
-            y.read(8)
-            loaded_x = np.fromfile(x, dtype=np.uint8,count=-1)/255
-            loaded_x = np.reshape(loaded_x,[-1,28,28,1])
-            loaded_y = np.fromfile(y, dtype=np.uint8)
-    #张量流
+    def load_x(dir):
+        x = open(dir,"rb")
+        x.read(16)  # 所在的位置开始读16个字节
+        loaded_x = np.fromfile(x, dtype=np.uint8, count=-1) / 255
+        loaded_x = np.reshape(loaded_x, [-1, 28, 28, 1])
+        x.close()
+        return loaded_x
+
+    def load_y(dir):
+        with open(dir, 'rb') as y:# 解析
+            y.read(8)#所在的位置8个 offset 开始字节
+            y = np.fromfile(y, dtype=np.uint8)
+        return y
+
+    loaded_x = load_x("./data/train-images-idx3-ubyte")
+    loaded_xte = load_x("./data/t10k-images-idx3-ubyte")
+    loaded_y = load_y('./data/train-labels-idx1-ubyte')
+    loaded_yte = load_y('./data/t10k-labels-idx1-ubyte')
+
+    loaded_y = onehot(loaded_y)
+    loaded_yte = onehot(loaded_yte)
 
     print('load complete!')
-    loady = onehot(loaded_y)
-    return loaded_x[:55000] ,loady[:55000],loaded_x[55000:60000],loady[55000:60000]
+    return loaded_x ,loaded_y,loaded_xte,loaded_yte
 
 if __name__ == '__main__':
 
     a,b ,c,d =  readdata()
-    imshow(a,10)
+    print(len(a))
+    print(len(b))
+    print(len(c))
+    print(len(d))
+    imshow(a,b,2)
